54fff6e300cb4625b55f9c0f116d45943cae414f
FrancisAcuna1 <francisacuna@thelewiscollege.edu.ph>
2023-11-14 21:45:02 -0800

    Second Commit adding product Item

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,89 @@
-import React  from "react";
+import React, { useState }  from "react";
 import Dashboard from "../components/dashboard";
 import Navbar from "../components/navbar";
-import { Box, width } from "@mui/system";
+import { Box } from "@mui/system";
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import { Button } from "@mui/material";
+import { styled } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+
 export default function Product(){
+    // const [count, setCount] = useState (1)
+    const [productlist, setProductList] = useState ([]); // List of Array
+
+    const [formdata, setFormData] = useState({
+        id: '',
+        product: '',
+        price: '',      
+        stocks: '',
+        category: '',
+    }); // object array
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); //in prevent sini na function na mag reload ang page 
+
+        // const newprod = ({
+        //     id: count,
+        //     product: '',
+        //     price: '',      
+        //     stocks: '',
+        //     category: '',
+        // })
+        setProductList([...productlist, formdata]) // dini in add ang mga data sa form
+        // setCount(count+1)
+        setFormData({
+            id: '',
+            product: '',
+            price: '',      
+            stocks: '',
+            category: '',
+        }); // In this part in reset lang niya sa empty ang mga form input once na trigger na ang button. 
+
+        console.log(formdata)
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+    
+        // Update the corresponding field in the form data
+        setFormData({
+          ...formdata,
+          [name]: value,
+        });
+      };
+    
+    
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+          backgroundColor: theme.palette.common.black,
+          color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+          fontSize: 14,
+        },
+    }));
+      
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+          border: 2,
+        },
+    }));
+
     return(
         <>  
             <Navbar/>
@@ -25,6 +102,7 @@ export default function Product(){
                         border: '2px solid black',
                         padding: '20px',
                     }}
+                    onSubmit={handleSubmit}
                     >
                     <Grid container spacing={3}>
                         <Grid item xs={11} sm={2}>
@@ -32,6 +110,9 @@ export default function Product(){
                             required
                             id="outlined-required"
                             label="ID"
+                            name="id"
+                            value={formdata.id}
+                            onChange={handleInputChange}
                             variant="outlined"
                             fullWidth
                            
@@ -42,6 +123,9 @@ export default function Product(){
                             required
                             id="outlined-required"
                             label="Product Name"
+                            name="product"
+                            value={formdata.product}
+                            onChange={handleInputChange}
                             variant="outlined"
                             fullWidth
                            
@@ -53,6 +137,9 @@ export default function Product(){
                             id="outlined-number"
                             label="Price"
                             type="number"
+                            name="price"
+                            value={formdata.price}
+                            onChange={handleInputChange}
                             InputLabelProps={{ shrink: true }}
                             variant="outlined"
                             fullWidth
@@ -65,6 +152,9 @@ export default function Product(){
                             id="outlined-number"
                             label="Stock"
                             type="number"
+                            name="stocks"
+                            value={formdata.stocks}
+                            onChange={handleInputChange}
                             InputLabelProps={{ shrink: true }}
                             variant="outlined"
                             fullWidth
@@ -76,6 +166,9 @@ export default function Product(){
                             required
                             id="outlined-required"
                             label="Category"
+                            name="category"
+                            value={formdata.category}
+                            onChange={handleInputChange}
                             variant="outlined"
                             fullWidth
                            
@@ -83,13 +176,59 @@ export default function Product(){
                         </Grid>
                         <Grid item xs={11} sm={2}>
                         <Stack direction="row" spacing={4} style={{width: "100%", alignItems: "center"}}>
-                            <Button variant="contained" sx={{p: 2, width: "100%", background:"black"}}>SUBMIT</Button>
+                            <Button variant="contained" sx={{p: 2, width: "100%", background:"black" }} type="submit">SUBMIT</Button>
                         </Stack>
                         </Grid>
                         
                     </Grid>
                     </form>
-                   
+
+                    <Box height={50}/>
+                    <Grid container />
+                    
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                            <TableHead>
+                            <TableRow>                            
+                                <StyledTableCell align="center">ID</StyledTableCell>
+                                <StyledTableCell align="center">PRODUCT</StyledTableCell>
+                                <StyledTableCell align="center">PRICE</StyledTableCell>
+                                <StyledTableCell align="center">STOCKS</StyledTableCell>
+                                <StyledTableCell align="center">CATEGORY</StyledTableCell>
+                                <StyledTableCell align="center">ACTION</StyledTableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {productlist.map((product, index) => (
+                                <StyledTableRow key={index}>
+                                <StyledTableCell align="center">{product.id}</StyledTableCell>
+                                <StyledTableCell align="center">{product.product}</StyledTableCell>
+                                <StyledTableCell align="center">{product.price}</StyledTableCell>
+                                <StyledTableCell align="center">{product.stocks}</StyledTableCell>
+                                <StyledTableCell align="center">{product.category}</StyledTableCell>
+                                <StyledTableCell align="center">
+                                    <Stack direction="row" spacing={1}  style={{width: "85%", alignItems: "center", height: 49, marginRight:"-180px" }}>
+                                        <Button variant="contained" size="small" color="success" sx={{p: 1, width: "50%",}} type="submit">EDIT</Button>
+                                        <Button variant="contained" size="small"  color="error" sx={{p: 1, width: "50%",}} type="submit">DELETE</Button>
+                                    </Stack>
+                                </StyledTableCell>
+                                
+                                </StyledTableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <Grid/>
+                 
+                    
+
+
+
+
+
+
+
+
                 </Box>
                
             </Box>
